35d67817ea7c27fa47594f8c1ff3d505a3989324
masal9pse <returnymgstokh@icloud.com>
2020-11-12 07:25:10 +0900

    ホスト名を省略した記述

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -29,7 +29,7 @@ export default {
     Logo
   },
   async asyncData(app) {
-    const data = await app.$axios.$get("http://localhost:8000/api");
+    const data = await app.$axios.$get("/api");
     return {
       data
     };
