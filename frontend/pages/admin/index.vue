9e0de7cd14fd122da2b039a6a5a1eaaf68623600
masal9pse <returnymgstokh@icloud.com>
2020-11-16 08:09:18 +0900

    編集用モーダル設置

--- frontend/pages/admin/index.vue
+++ frontend/pages/admin/index.vue
@@ -16,6 +16,40 @@
         class="elevation-1"
       >
         <!-- 追加 -->
+        <template v-slot:top>
+          <v-dialog v-model="isShowDialog" max-width="500px">
+            <v-card>
+              <v-card-title>
+                <span class="headline">管理者編集</span>
+              </v-card-title>
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col cols="6">
+                      <v-text-field v-model="dialogAdminUser.email" label="メールアドレス" />
+                    </v-col>
+                    <v-col cols="6">
+                      <v-text-field v-model="dialogAdminUser.name" label="名前" />
+                    </v-col>
+                    <v-col cols="12">
+                      <v-text-field
+                        type="password"
+                        v-model="dialogAdminUser.password"
+                        label="パスワード"
+                      />
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn @click="closeDialog">閉じる</v-btn>
+                <v-btn class="primary" @click="onClickUpdateBtn">更新する</v-btn>
+                <v-spacer />
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </template>
         <template v-slot:item.edit-action="{ item }">
           <v-icon small @click="onClickEditIcon(item)">mdi-pencil</v-icon>
         </template>
@@ -34,9 +68,7 @@ export default {
   },
   data() {
     return {
-      //追加
       dialogAdminUser: {},
-      //追加
       isShowDialog: false,
       searchText: ""
     };
@@ -55,11 +87,20 @@ export default {
       ];
     }
   },
-  //追加
   methods: {
+    //追加
+    closeDialog() {
+      this.dialogAdminUser = {};
+      this.isShowDialog = false;
+    },
     onClickEditIcon(adminUser) {
       this.dialogAdminUser = Object.assign({}, adminUser);
       this.isShowDialog = true;
+    },
+    //追加
+    async onClickUpdateBtn() {
+      await this.$store.dispatch("adminUsers/update", this.dialogAdminUser);
+      this.closeDialog();
     }
   }
 };
